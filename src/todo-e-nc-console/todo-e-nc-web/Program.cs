917eb4400b2c8c7c658cf1042763e126a6ce44bf
Yanesh Tyagi <yanesh.tyagi@gmail.com>
2020-04-26 00:58:01 +0530

    Activate Electron by callung UseElectron method in program.cs

--- src/todo-e-nc-console/todo-e-nc-web/Program.cs
+++ src/todo-e-nc-console/todo-e-nc-web/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ElectronNET.API;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -21,6 +22,7 @@ namespace todo_e_nc_web
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseElectron(args);
                 });
     }
 }
