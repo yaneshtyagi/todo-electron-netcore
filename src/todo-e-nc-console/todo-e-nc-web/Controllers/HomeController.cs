c29a61f3f2d586f61633fbc5e0900fbc0afc7c4d
Yanesh Tyagi <yanesh.tyagi@gmail.com>
2020-04-26 09:04:11 +0530

    implemented inter process communication

--- src/todo-e-nc-console/todo-e-nc-web/Controllers/HomeController.cs
+++ src/todo-e-nc-console/todo-e-nc-web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ElectronNET.API;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using todo_e_nc_web.Models;
@@ -20,6 +21,11 @@ namespace todo_e_nc_web.Controllers
 
         public IActionResult Index()
         {
+            Electron.IpcMain.On("async-msg", (data) =>
+            {
+                var mainWindow = Electron.WindowManager.BrowserWindows.First();
+                Electron.IpcMain.Send(mainWindow, "asynchronous-reply", "pong");
+            });
             return View();
         }
 
